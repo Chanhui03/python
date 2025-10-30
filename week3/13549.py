386b2f2c0442e91fe02807df518f4ee3d81a48e5
Chanhui03 <kw003004@gmail.com>
2025-10-30 14:39:39 +0900

    .

--- week3/13549.py
+++ week3/13549.py
@@ -8,16 +8,23 @@ from collections import deque
 n, k = map(int, input().split())
 limit = 100001
 cnt = [0] * limit
+
+# 방문시간 기록 배열
 visited = [False] * limit
 
 def bfs(x, end):
     dq = deque()
+    # 시작위치 x
     dq.append(x)
 
+    # dq 반복
     while dq:
         x = dq.popleft()
+
+        # 목표 위치 도달 시 종료
         if x == end:
             return cnt[x]
+        # 우선 순위 x*2(0초) > x-1(1초) > x+1(1초)
         if -1 < x * 2 < limit and visited[x * 2] == 0:
             dq.appendleft(x * 2)
             cnt[x * 2] = cnt[x]
