66ded4a190c41f5086e95978bc68269534e445fc
Chanhui03 <kw003004@gmail.com>
2025-11-24 00:37:29 +0900

    세 용액

---
+++ week7/2473.py
@@ -0,0 +1,31 @@
+# 세 용액
+import sys
+input = sys.stdin.readline
+
+N = int(input())
+# 용액 특성값들 입력받기
+list = sorted(list(map(int, input().split())))
+
+# 임의의 max값
+result = 4000000000
+# 해답 용액 3개
+solve = []
+
+for i in range(N - 2):
+    refer = list[i]
+    left_pointer = i + 1
+    right_pointer = N - 1
+    while left_pointer < right_pointer:
+        current_sum = refer + list[left_pointer] + list[right_pointer]
+        if abs(current_sum) <= abs(result):
+            solve = [refer, list[left_pointer], list[right_pointer]]
+            result = refer + list[left_pointer] + list[right_pointer]
+        if current_sum < 0:
+            left_pointer += 1
+        elif current_sum > 0:
+            right_pointer -= 1
+        else:
+            break
+
+for i in solve:
+    print(i, end = " ")
